7d3eece0894a0ad14f584581dec9bf619504b828
Gishman-creator <147145228+Gishman-creator@users.noreply.github.com>
2024-08-06 23:24:11 +0200

    eighth commit
    fixed the islogged in state

--- app/front-end/src/App.jsx
+++ app/front-end/src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import axiosUtils from './utils/axiosUtils'; // Adjust path as necessary
@@ -8,7 +7,7 @@ import Admin from './features/admin/Admin';
 import Authentication from './features/authentication/Authentication';
 
 const App = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(null); // Start with null to indicate loading state
 
     useEffect(() => {
         const validateTokens = async () => {
@@ -16,28 +15,27 @@ const App = () => {
             const refreshToken = localStorage.getItem('refreshToken');
             console.log('accessToken:', accessToken);
             console.log('refreshToken:', refreshToken);
-        
+
             if (!accessToken || !refreshToken) {
                 setIsLoggedIn(false);
                 return;
             }
-        
+
             try {
                 const response = await axiosUtils('/api/auth/validate-tokens', 'POST', {}, {
                     Authorization: `Bearer ${accessToken}`,
-                    'x-refresh-token': refreshToken
+                    'x-refresh-token': refreshToken,
                 });
-        
+
                 console.log('Token validation response:', response);
-        
+
                 if (response.status === 200) {
-                    const newAccessToken = response.data.accessToken; // Access the new token from response body
+                    const newAccessToken = response.data.accessToken; // Ensure `accessToken` is in response.data
                     if (newAccessToken) {
                         localStorage.setItem('accessToken', newAccessToken);
                         console.log('New access token received and stored');
                     }
                     setIsLoggedIn(true);
-                    console.log('Token validation successful');
                 } else {
                     console.log('Token validation failed');
                     setIsLoggedIn(false);
@@ -47,10 +45,14 @@ const App = () => {
                 setIsLoggedIn(false);
             }
         };
-        
 
         validateTokens();
     }, []);
+
+    if (isLoggedIn === null) {
+        // Render a loading state while checking the token
+        return <div className="flex justify-center items-center min-h-screen"></div>;
+    }
 
     return (
         <Router>

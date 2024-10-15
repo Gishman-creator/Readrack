0499684eec6c7a2574b1e6e2d410536feb775851
Gishman-creator <147145228+Gishman-creator@users.noreply.github.com>
2024-10-15 10:22:47 +0200

    forty-third commit
    made database partitions

--- app/front-end/src/features/admin/components/Banner.jsx
+++ app/front-end/src/features/admin/components/Banner.jsx
@@ -13,11 +13,11 @@ function Banner() {
         // Make the initial API call to start the update process
         const startUpdate = async () => {
             try {
-                // const validateAuthorsResponse = await axiosUtils('/api/validateAuthors', 'POST');
-                // console.log('Validate authors response:', validateAuthorsResponse);
+                const validateAuthorsResponse = await axiosUtils('/api/validateAuthors', 'POST');
+                console.log('Validate authors response:', validateAuthorsResponse);
 
-                const updateAuthorRatingsResponse = await axiosUtils('/api/updateAuthorRatings', 'POST');
-                console.log('Update author ratings response:', updateAuthorRatingsResponse);
+                // const updateAuthorRatingsResponse = await axiosUtils('/api/updateAuthorRatings', 'POST');
+                // console.log('Update author ratings response:', updateAuthorRatingsResponse);
             } catch (error) {
                 console.error('Error starting author update:', error);
             }
@@ -26,22 +26,22 @@ function Banner() {
         startUpdate();
 
         // Listen for progress updates from the WebSocket
-        // socket.on('validateAuthorProgress', (data) => {
-        //     setValidateAuthorProgress(data);  // Update progress percentage
-        // });
+        socket.on('validateAuthorProgress', (data) => {
+            setValidateAuthorProgress(data);  // Update progress percentage
+        });
 
-        // socket.on('validateMessage', (message) => {
-        //     setValidateMessage(message);  // Update progress percentage
-        // });
+        socket.on('validateMessage', (message) => {
+            setValidateMessage(message);  // Update progress percentage
+        });
 
         // Listen for progress updates from the WebSocket
-        socket.on('authorRatingsProgress', (data) => {
-            setAuthorRatingsProgress(data);  // Update progress percentage
-        });
+        // socket.on('authorRatingsProgress', (data) => {
+        //     setAuthorRatingsProgress(data);  // Update progress percentage
+        // });
 
-        socket.on('ratingMessage', (message) => {
-            setRatingMessage(message);  // Update progress percentage
-        });
+        // socket.on('ratingMessage', (message) => {
+        //     setRatingMessage(message);  // Update progress percentage
+        // });
 
         // Clean up socket connection when the component unmounts
         return () => {
@@ -54,8 +54,8 @@ function Banner() {
 
     return (
         <div className="flex justify-evenly w-full p-2 bg-primary text-white text-center text-sm font-emibold">
-            {/* <span>{validateMessage ? validateMessage : `Author validation: ${validateAuthorProgress}`}</span> */}
-            <span>{ratingMessage ? ratingMessage : `Author rating: ${authorRatingsProgress}`}</span>
+            <span>{validateMessage ? validateMessage : `Author validation: ${validateAuthorProgress}`}</span>
+            {/* <span>{ratingMessage ? ratingMessage : `Author rating: ${authorRatingsProgress}`}</span> */}
         </div>
     );
 }

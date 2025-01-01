361c8f24d434c36726725d67e3b4159cddfb82a4
Gishman-creator <147145228+Gishman-creator@users.noreply.github.com>
2025-01-01 14:46:28 +0200

    testing the ban of console.log in production

--- app/front-end/src/App.jsx
+++ app/front-end/src/App.jsx
@@ -6,6 +6,15 @@ import Admin from './features/admin/Admin';
 import Authentication from './features/authentication/Authentication';
 import NotFoundPage from './pages/NotFoundPage';
 
+if (import.meta.env.MODE === 'production') {
+    console.log = function () {};
+}
+
+// Now you can use console.log as normal in development, but it will be ignored in production.
+// console.error will still work as usual.
+console.log("This will not show in production");
+console.error("This will show in both development and production");
+
 const App = () => {
     return (
         <div className='h-screen-nonav bg-[#f9f9f9]'>
